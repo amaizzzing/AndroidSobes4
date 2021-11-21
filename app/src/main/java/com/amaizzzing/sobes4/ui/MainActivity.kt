4ba8d6346f119075e0ee415bb4e80c8099011691
amaizzzing <amaizzzing@gmail.com>
2021-11-21 14:18:10 +0500

    commit

--- app/src/main/java/com/amaizzzing/sobes4/ui/MainActivity.kt
+++ app/src/main/java/com/amaizzzing/sobes4/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.amaizzzing.sobes4
+package com.amaizzzing.sobes4.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.amaizzzing.sobes4.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
